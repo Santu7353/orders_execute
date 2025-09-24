ef684b24a7f9f89e272580bff409b6c7add695be
santosh <santoshshivapuri09@gmail.com>
2025-09-24 16:17:56 +0530

    Added graph data file

---
+++ src/main/java/GraphData.java
@@ -0,0 +1,17 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class GraphData {
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+    }
+
+}
